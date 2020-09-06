5e9ca3ce1b922ccde79c8a1ec64b9b717942abb5
ahmed alaa <ahmed.alaasoliman@gmail.com>
2020-09-06 05:13:49 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,11 +4,11 @@
 
 # Step 1:
 # This is your Docker ID/path
-dockerpath=manishbisht/development
+dockerpath=24114187/devops_udacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run development --image=manishbisht/development --port=80
+kubectl run devops_udacity --image=24114187/devops_udacity --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,5 +16,5 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment development --type=LoadBalancer --port=8000 --target-port=80
+kubectl expose deployment devops_udacity --type=LoadBalancer --port=8000 --target-port=80
 
