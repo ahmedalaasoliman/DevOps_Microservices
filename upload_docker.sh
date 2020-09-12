8bc0ad2d04b39ef4f9335794f881f83c9078c9ec
ahmed alaa <ahmed.alaasoliman@gmail.com>
2020-09-12 17:37:49 +0200

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
-
-# Assumes that an image is built via `run_docker.sh`
-
 # Step 1:
 # Create dockerpath
 dockerpath=24114187/devops_udacity
-
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker tag cdfeb2e7e284 $dockerpath
+docker tag latest 24114187/devops_udacity
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath
+docker push docker.io/24114187/devops_udacity:latest
